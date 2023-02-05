ea9d6fe8cf9708cb8587313641e75066ecbe3936
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-02-05 16:36:08 +0530

    Add files via upload

---
+++ 2_variable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a=3; //%d
+    float b=3.4;  //f
+    char c='r';  //c
+    int d=45;
+    printf("the value of a is %d \n",a);  //the value of a is 3
+    printf("the value of b is %f \n",b);
+    printf("the value of c is %c \n",c);
+    printf("%d",a+d);
+    printf("%d",a-d);
+    return 0; 
+}
+// for comment=="//"
+// more than one=/*ggfggfffg*/
+// character == single quote
