028d63043ec72c7226662c0794d05259d35e918b
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-02-05 22:12:01 +0530

    Add files via upload

---
+++ 4_type_instruction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    // int a=4,b,c;  //type declaration
+    // b=c=a;
+    // printf("%d \n",a);
+    // printf("%d \n",b);
+    // printf("%d \n",c);
+    float a=1.1;
+    float b=a+9.8;
+    int c,d;
+    c=d=30;
+    printf("%f \n",b);
+    printf("%d \n",c);
+    printf("%d \n",d);
+    return 0;
+}
