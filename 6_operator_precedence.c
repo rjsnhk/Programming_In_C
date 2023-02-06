1eceaf980fb3fee8df87e3e289d1cd343e45a3f5
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-02-06 20:11:34 +0530

    Add files via upload

---
+++ 6_operator_precedence.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    // *,/,%<--->+,-,<-->=
+    //if *,/ ek sath then left to right solve krna he
+    int x = 2;
+    int y = 3;
+    printf("the valu of 8*y/3*x is %d \n",8*y/3*x); //first 8*3 then devide by 3 then multi by 2 --16 not 24/6
+    return 0;
+}
