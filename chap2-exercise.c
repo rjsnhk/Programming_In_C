6c69bbdc39708d47332612d84266e112e5b1418f
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-02-06 20:11:43 +0530

    Add files via upload

---
+++ chap2-exercise.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    // Q1
+    //  int a; b=a;  //invalid
+    //  int v=3^3;  //valid
+    //  char dt='21 dec 2020'; //only ek hi character
+    //  printf("%c",dt);
+
+    // Q2
+    // printf("%f\n", 3.0 / 8 - 2);
+
+    // Q3
+    // int num;
+    // printf("enter num \n");
+    // scanf("%d",&num);
+    // printf("divisible test of 97 is %d \n",num%97);
+
+    // Q4
+    // int x=2;
+    // int y=3;
+    // int z=3;
+    // int k=1;
+    // printf("%d\n",3*x);
+    // printf("%d\n",3*x/y);
+    // printf("%d\n",3*x/y-z);
+    // printf("%d\n",3*x/y-z+k);
+
+
+    //Q5
+    // printf("%f",3.0+1); //floating point
+
+
+    return 0;
+}
