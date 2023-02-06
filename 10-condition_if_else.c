9ba0a20ec1685ce22003692407b241964763de90
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-02-06 23:43:23 +0530

    Add files via upload

---
+++ 10-condition_if_else.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("enter num \n");
+    scanf("%d",&a);
+
+    (a<5) ? printf("a is less than 5") : printf("a is greated than 5"); 
+    return 0;
+}
