f5ebbdf9604da047ef555024111fca9433ba83ba
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-02-16 23:38:26 +0530

    Update 2_variable.c

--- 2_variable.c
+++ 2_variable.c
@@ -7,7 +7,7 @@ int main(){
     printf("the value of a is %d \n",a);  //the value of a is 3
     printf("the value of b is %f \n",b);
     printf("the value of c is %c \n",c);
-    printf("%d",a+d);
+    printf("%d",a*d);
     printf("%d",a-d);
     return 0; 
 }
