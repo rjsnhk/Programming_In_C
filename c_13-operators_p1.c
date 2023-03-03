f753e8f20c8bfcbcbbf54e4759278e697c4ab4ec
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-03 20:00:59 +0530

    Add files via upload

---
+++ c_13-operators_p1.c
@@ -0,0 +1,22 @@
+//type of operator based on operands--
+//1=unary(only one operand use)
+//+,-,like a + (-b)
+// ++,--
+// (! (logical not),&(address),size of)
+
+
+
+//2=binary(2 operand)==
+//arithmatic(+,-,/,*,%)
+//relational(<,>,<=,>=)
+//logical(&&,||)
+//bitwise(&,|,<<,>>,^,~)
+//equality(==,!=)
+//comma(,)
+//assignment (=)
+
+
+//3=ternary(3 operand)
+//(?:)==if else case
+//if (a>b)?printf("true"):printf("false")
+
