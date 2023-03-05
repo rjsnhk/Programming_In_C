29ddcd0878bc51b9c0710842b48ef3cb0f4d1cc6
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-05 13:15:57 +0530

    Add files via upload

---
+++ c_20-comma_operator.c
@@ -0,0 +1,20 @@
+// bitwise(<<(left),>>(right),~)
+
+#include <stdio.h>
+int main(){
+    int a=10,b,c,d,m=8,n,n2;
+    b=(2,3,4);
+    // c=(2,3,4,printf("hello"),5,6); //first hello print hoga baki uske niche b,c
+    // printf("%d",b);
+    // printf("%d",c);
+    d=(2,3,4,printf("hiihkuhkjhjkhjkz"));
+    printf("%d\n",d); // string+no of character
+    n=(m++,++m);
+    printf("%d",n); //10
+    printf("%d",m); //10
+    n2=m++,++m;
+    printf("%d",n2); //10
+    printf("%d",m); //12
+
+    return 0;
+}
