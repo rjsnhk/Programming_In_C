759c26409096e84612dfb1ec64a60198f897dbcb
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-05 17:17:12 +0530

    Add files via upload

---
+++ c_23-formated_input_funcn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    float b;
+    int a,c;
+    // scanf("%f",&b);
+    // printf("%d",b);
+    // scanf("%f",&b);
+    // printf("%d",b);
+    // scanf("%f",&b);
+    // printf("%d",b);
+    scanf("%f%d",&a,&c);
+    printf("%d%d",a,c);
+    return 0;
+}
